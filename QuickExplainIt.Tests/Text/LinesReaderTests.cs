9afcc68b7f5297c291372f0ee9abe5dc197d393f
kilfour <kilfour@gmail.com>
2025-07-12 21:36:45 +0200

    doc fix

--- QuickExplainIt.Tests/Text/LinesReaderTests.cs
+++ QuickExplainIt.Tests/Text/LinesReaderTests.cs
@@ -9,10 +9,9 @@ var reader = LinesReader.FromText(vcardString);
 Assert.Equal(""BEGIN:VCARD"", reader.NextLine());
 reader.Skip();
 Assert.True(reader.EndOfContent());
-
+```
 Use `FromText(...)` for newline-separated text or `FromStringList(...)` for an array of lines. 
-It throws exceptions when reading past the end or using it uninitialized.
-```")]
+It throws exceptions when reading past the end or using it uninitialized.")]
 public class LinesReaderTests
 {
     [Fact]
